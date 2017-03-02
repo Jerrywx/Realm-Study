36af0dc1cf94a747eb106ff29ee00132e90fe4f7
Jerrywx <23601145@qq.com>
2017-03-02 08:35:41 +0800

    添加网络模块

---
+++ SwiftDown/SwiftDown/Product/JRModule/JRNetManager/JRNetWorkManager.swift
@@ -0,0 +1,23 @@
+//
+//  JRNetWorkManager.swift
+//  SwiftDown
+//
+//  Created by 王潇 on 2017/3/2.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class JRNetWorkManager: NSObject {
+
+	func asdasd() {
+		JRNetWorkURL.aa
+	}
+}
+
+// MARK: - 公共上行参数
+extension JRNetWorkManager {
+	
+	
+	
+}
---
+++ SwiftDown/SwiftDown/Product/JRModule/JRNetManager/JRNetWorkURL.swift
@@ -0,0 +1,15 @@
+//
+//  JRNetWorkURL.swift
+//  SwiftDown
+//
+//  Created by 王潇 on 2017/3/2.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class JRNetWorkURL: NSObject {
+	
+	static let aa = "asd"
+
+}
