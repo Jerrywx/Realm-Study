f7e7aec48c216784091e29aa16fadf25f6f17abb
Jerrywx <23601145@qq.com>
2017-03-03 10:22:21 +0800

    添加ignore文件

---
+++ SwiftDown/SwiftDown/Product/JRModule/JRNetManager/JRIgnoreFile.swift
@@ -0,0 +1,13 @@
+//
+//  JRIgnoreFile.swift
+//  SwiftDown
+//
+//  Created by 王潇 on 2017/3/3.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class JRIgnoreFile: NSObject {
+
+}
