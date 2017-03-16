3c9717abd9c8ac2ef2ba27cc2e21c6432d1a0c2f
Jerrywx <23601145@qq.com>
2017-03-17 02:26:00 +0800

    web loading

--- SwiftDown/SwiftDown/Product/JRModule/JRWebKitModule/JRWebView.swift
+++ SwiftDown/SwiftDown/Product/JRModule/JRWebKitModule/JRWebView.swift
@@ -7,12 +7,14 @@
 //
 
 import WebKit
+import MBProgressHUD
 
 class JRWebView: WKWebView {
 	
 	override init(frame: CGRect, configuration: WKWebViewConfiguration) {
 		super.init(frame: frame, configuration: configuration)
-		
+		self.uiDelegate = self
+		self.navigationDelegate = self
 	}
 	
 	required init?(coder: NSCoder) {
@@ -33,5 +35,52 @@ extension JRWebView {
 		///
 		load(request as URLRequest)
 	}
+}
+
+// MARK: - WKNavigationDelegate
+extension JRWebView: WKNavigationDelegate {
+	
+	/// 开始加载WebView
+	///
+	/// - Parameters:
+	///   - webView: 当前webView
+	///   - navigation: webView 导航
+	func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+		MBProgressHUD.showAdded(to: webView, animated: true)
+	}
+	
+	/// webView 加载完成
+	///
+	/// - Parameters:
+	///   - webView: 当前webView
+	///   - navigation: webView 导航
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		MBProgressHUD.hide(for: webView, animated: true)
+	}
+	
+	/// webView 加载失败
+	///
+	/// - Parameters:
+	///   - webView: 当前 webView
+	///   - navigation: webview 导航
+	///   - error: 错误原因
+	func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+		MBProgressHUD.hide(for: webView, animated: true)
+	}
+	
+	func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+		
+		print(navigationResponse.response.url ?? "error")
+		decisionHandler(.allow);
+	}
 	
 }
+
+// MARK: - WKUIDelegate
+extension JRWebView: WKUIDelegate {
+	
+	
+	
+}
+
+
