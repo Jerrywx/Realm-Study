201667b5c13daa75b664964347e1072757fcd0c5
Jerrywx <23601145@qq.com>
2017-03-27 18:34:43 +0800

    提交代码

---
+++ SwiftDown/SwiftDown/Product/JRSwiftModule/SwiftCollectionType/SwiftCollectionType.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftCollectionType.swift
+//  SwiftDown
+//
+//  Created by wxiao on 2017/3/27.
+//  Copyright © 2017年 王潇. All rights reserved.
+//
+
+import UIKit
+
+class SwiftCollectionType: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+    }
+}
